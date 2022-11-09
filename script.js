884660f4046bf44dfce51cf522c91573bda00e45
Guilherme <99549092+GuilhermeDJM@users.noreply.github.com>
2022-11-08 21:20:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function redirectDiscord(){
+    location = "https://discord.com/channels/@me/1036345957811494992"
+}
+
+function redirectInstagram(){
+    location = "https://www.instagram.com/shadowblack_dev/"
+}
+
+function redirectTwitter(){
+    location = "https://twitter.com/ShadowBlack_dev"
+}
+
+function redirectProgrammerSite(){
+    location = "programmingContext/index.html"
+}
+
+function redirectModelerSite(){
+    location = "modellingContext/index.html"
+}
+
+function redirectDesignerSite(){
+    location = "designerContext/index.html"
+}
